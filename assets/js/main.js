dbc360a3f120c029a3d08775f35edcdb86113956
ohara <w2f.8018.ohara@gmail.com>
2021-07-27 21:39:35 +0900

    maim.js削除

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-export function hideElement(selector) {
-    $(document).on('ready', function() {
-        $(".slider").slick({
-            autoplay: true,
-            autoplaySpeed: 4000,
-            dots: true,
-        });
-    });
-}
